e91b99b23ddbbb82b704b738af540c94de6958b2
linmingxiao <lmx_dlut@126.com>
2021-11-18 13:54:05 +0800

    单元测试

--- connx/sqlx/sqlx_test.go
+++ connx/sqlx/sqlx_test.go
@@ -1,1 +1,258 @@
 package sqlx
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"strings"
+)
+
+var db *sqlx.DB
+
+//链接DB
+func initDB() (err error) {
+	dsn := "root:trf.d.z@tcp(10.10.13.55:3306)/gozero?charset=utf8mb4&parseTime=True&loc=Local"
+	// 也可以使用MustConnect连接不成功就panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect DB failed, err:%v\n", err)
+		return
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	return
+}
+
+//表结构
+//为了方便演示插入数据操作，这里创建一个user表，表结构如下：
+//CREATE TABLE `user` (
+//`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+//`name` VARCHAR(20) DEFAULT '',
+//`age` INT(11) DEFAULT '0',
+//PRIMARY KEY(`id`)
+//)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+type User struct {
+	ID int `db:"id"`
+	Name string `db:"name"`
+	Age string `db:"age"`
+}
+
+// 查询单条数据示例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u User
+	err := db.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.ID, u.Name, u.Age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	var users []User
+	err := db.Select(&users, sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("users:%#v\n", users)
+}
+
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "沙河小王子", 19)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新数据
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 39, 6)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
+
+
+//DB.NamedExec方法用来绑定SQL语句与结构体或map中的同名字段。
+func insertUserDemo()(err error){
+	sqlStr := "INSERT INTO user (name,age) VALUES (:name,:age)"
+	_, err = db.NamedExec(sqlStr,
+		map[string]interface{}{
+			"name": "七米",
+			"age": 28,
+		})
+	return
+}
+
+//与DB.NamedExec同理，这里是支持查询。
+func namedQuery(){
+	sqlStr := "SELECT * FROM user WHERE name=:name"
+	// 使用map做命名查询
+	rows, err := db.NamedQuery(sqlStr, map[string]interface{}{"name": "七米"})
+	if err != nil {
+		fmt.Printf("db.NamedQuery failed, err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next(){
+		var u User
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+
+	u := User{
+		Name: "七米",
+	}
+	// 使用结构体命名查询，根据结构体字段的 db tag进行映射
+	rows, err = db.NamedQuery(sqlStr, u)
+	if err != nil {
+		fmt.Printf("db.NamedQuery failed, err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next(){
+		var u User
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+}
+
+
+//对于事务操作，我们可以使用sqlx中提供的db.Beginx()和tx.Exec()方法。示例代码如下：
+func transactionDemo2()(err error) {
+	tx, err := db.Beginx() // 开启事务
+	if err != nil {
+		fmt.Printf("begin trans failed, err:%v\n", err)
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			fmt.Println("rollback")
+			tx.Rollback() // err is non-nil; don't change it
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+			fmt.Println("commit")
+		}
+	}()
+
+	sqlStr1 := "Update user set age=20 where id=?"
+
+	rs, err := tx.Exec(sqlStr1, 1)
+	if err!= nil{
+		return err
+	}
+	n, err := rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return errors.New("exec sqlStr1 failed")
+	}
+	sqlStr2 := "Update user set age=50 where i=?"
+	rs, err = tx.Exec(sqlStr2, 5)
+	if err!=nil{
+		return err
+	}
+	n, err = rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return errors.New("exec sqlStr1 failed")
+	}
+	return err
+}
+
+
+//使用NamedExec实现批量插入：
+func BatchInsertUsers(users []*User) error {
+	_, err := db.NamedExec("INSERT INTO user (name, age) VALUES (:name, :age)", users)
+	return err
+}
+
+
+//in查询
+//查询id在给定id集合中的数据。
+// QueryByIDs 根据给定ID查询
+func QueryByIDs(ids []int)(users []User, err error){
+	// 动态填充id
+	query, args, err := sqlx.In("SELECT name, age FROM user WHERE id IN (?)", ids)
+	if err != nil {
+		return
+	}
+	// sqlx.In 返回带 `?` bindvar的查询语句, 我们使用Rebind()重新绑定它
+	query = db.Rebind(query)
+	err = db.Select(&users, query, args...)
+	return
+}
+
+//in查询和FIND_IN_SET函数
+//查询id在给定id集合的数据并维持给定id集合的顺序。
+// QueryAndOrderByIDs 按照指定id查询并维护顺序
+func QueryAndOrderByIDs(ids []int)(users []User, err error){
+	// 动态填充id
+	strIDs := make([]string, 0, len(ids))
+	for _, id := range ids {
+		strIDs = append(strIDs, fmt.Sprintf("%d", id))
+	}
+	query, args, err := sqlx.In("SELECT name, age FROM user WHERE id IN (?) ORDER BY FIND_IN_SET(id, ?)", ids, strings.Join(strIDs, ","))
+	if err != nil {
+		return
+	}
+
+	// sqlx.In 返回带 `?` bindvar的查询语句, 我们使用Rebind()重新绑定它
+	query = db.Rebind(query)
+
+	err = db.Select(&users, query, args...)
+	return
+}
